ce932804c95699f6841c0e4301f8b91777a695e6
mahfuzahmmedabir <mahfuz.abir06@gmail.com>
2025-05-12 11:58:43 +0100

    2 D Array intput and output

---
+++ 2D_array.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int arr[r][c];
+    
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    } 
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            printf("%d ", arr[i][j]);
+        }
+         printf("\n");
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+    // int a;
+    // scanf("%d",&a);
+    // int arr[a];
+    // for(int i = 0;i<a; i++){
+    //     scanf("%d", &arr[i]);
+    // }
+    // for(int i = 0;i<a; i++){
+    //     printf("%d ", arr[i]);
+    // }
+    
+   
+   return 0;
+}
