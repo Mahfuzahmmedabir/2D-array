28ed7cb926977502fad36faf759cc95072275880
mahfuzahmmedabir <mahfuz.abir06@gmail.com>
2025-05-12 15:50:40 +0100

    print specific row and col

--- 2D_array.c
+++ 2D_array.c
@@ -5,20 +5,35 @@ int main()
     int r,c;
     scanf("%d %d",&r,&c);
     int arr[r][c];
-    
+
+  
+
     for(int i = 0; i < r; i++){
         for(int j = 0; j < c; j++){
             scanf("%d", &arr[i][j]);
         }
     } 
-    for(int i = 0; i < r; i++){
-        for(int j = 0; j < c; j++){
-            printf("%d ", arr[i][j]);
-        }
-         printf("\n");
+    int spscipic_col ;
+    scanf("%d",&spscipic_col);
+
+    // for(int i = 0;i <c; i++){
+    //     printf("%d ",arr[spscipic_row][i]);
+    // }
+
+    for(int i = 0; i<r; i++){
+        printf("%d ",arr[i][spscipic_col]);
     }
 
-    
+
+
+   
+  
+//  for(int i = 0; i < r; i++){
+//         for(int j = 0; j < c; j++){
+//             printf("%d ", arr[i][j]);
+//         }
+//          printf("\n");
+//     }
 
 
 
